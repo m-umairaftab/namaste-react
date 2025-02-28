cbb19b058ae8a4a9d51d458c4b751f35b0a2b5cd
Umair Aftab <m.umair.umairAftab@gmail.com>
2025-02-28 16:23:03 +0500

    episode 3

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "Heading 1"
+const Title = () => (
+  <h1 className="head" tabIndex={"5"}>
+    Namaste React Using JSX 🚀
+  </h1>
 );
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "Heading 2"
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading2]
-);
-console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//passing a react element inside the root
-//async defer
-root.render(container);
+// passing a react element inside the root
+// async defer
+root.render(<HeadingComponent />);
